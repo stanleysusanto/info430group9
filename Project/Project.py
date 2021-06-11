6c24b46f9f8a08935cd83d3db14ff9016609b472
Jason Jung <gokujung3@gmail.com>
2021-06-10 21:28:30 -0700

    Need to fix Connection

--- Project/Project.py
+++ Project/Project.py
@@ -6,47 +6,48 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd 
 import pyodbc
 
-urls = []
 csv_filepath = os.path.join(os.path.dirname(__file__), 'output.csv')    
 
 def scrape_urls():
-    for i in range(0, 100):
-        books = []
-        url = 'https://indyschild.com/100-things-to-do-outside-this-summer-at-home/'
-        print('Scraping url', url)
-        urlPage = requests.get(url)
-        soup = bs(urlPage.content, 'html.parser')
+    activities = []
+    url = 'https://indyschild.com/100-things-to-do-outside-this-summer-at-home/'
+    print('Scraping url', url)
+    urlPage = requests.get(url)
+    soup = bs(urlPage.content, 'html.parser')
 
-        Name = ''
-        try:
-            Name = soup.find_all('span', attrs={"style": "font-weight: 400;"})
-        except:
-            print('Could not scrape url: ', url)        
+    activity = []
+    try: 
+        activity = soup.find_all("li", attrs={"style": "font-weight: 400;"})
+    except:
+        print("cant do it")   
 
-        books.append([Name])
-        time.sleep(1)
+    for i in range(len(activity)):
+        newString = activity[i]
 
-        columns = ['Name']
-        print(Name)
-        df = pd.DataFrame(books, columns=columns)
-        
-        df.to_csv(csv_filepath, index = False)
+    activities.append([activity])
+    time.sleep(1)
+
+    columns = ['ActivityName']
+    df = pd.DataFrame(activities, columns=columns)
+    
+    df.to_csv(csv_filepath, index = False)
+    
 
 def insert_to_db():
     # Read our csv we created earlier but tell Pandas to not mess with 'N/A' values
     df = pd.read_csv(csv_filepath, keep_default_na=False)
 
-    server = 'is-info430.ischool.uw.edu'
-    database = 'HW4'
-    username = 'INFO430'
-    password = 'SuperSafePassword1234'
+    server = 'info430group9.database.windows.net'
+    database = 'INFO430_GROUP_PROEJCT'
+    username = 'info430group9'
+    password = 'info430group9'
     conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = conn.cursor()
     for index, row in df.iterrows():
-        cursor.execute("INSERT INTO dbo.jj_Books (Name,Price,UPC,InStock,Type) values(?,?,?,?,?)", row.Name, row.Price, row.UPC, row.InStock, row.Type)
+        cursor.execute("INSERT INTO dbo.ACTIVITY_SUMMER (ActivityName) values(?)", row.ActivityName)
     conn.commit()
     cursor.close()
 
 if __name__ == '__main__':
     scrape_urls()
-    # insert_to_db()
+    insert_to_db()
